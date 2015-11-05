0ac5bc3705138503b88bda6eef6d65acb2a6d6aa
David Goldberg <drgold1988@gmail.com>
2015-11-05 11:45:30 -0800

    [Test] adds unit testing for adding products to immutable state tree

---
+++ server/test/core_spec.js
@@ -0,0 +1,30 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setProducts} from '../src/core';
+
+describe('application logic', () => {
+
+  describe('setProducts', () => {
+
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const products = List.of('Body Wash', 'Hand Soap');
+      const nextState = setProducts(state, products);
+      expect(nextState).to.equal(Map({
+        products: List.of('Body Wash', 'Hand Soap')
+      }));
+    });
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const products = ['Body Wash', 'Hand Soap'];
+      const nextState = setProducts(state, products);
+      expect(nextState).to.equal(Map({
+        products: List.of('Body Wash', 'Hand Soap')
+      }));
+    });
+
+  });
+
+});
