5b987017f2225a65679c524101e49ff38cd148e8
David Goldberg <drgold1988@gmail.com>
2015-11-07 10:49:05 -0800

    [Feature] provides state to App root component which is pure and passed down via props

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,6 @@ const products = List.of('Body Wash', 'Hand Soap');
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {products: products});
+    return this.props.children;
   }
 })
