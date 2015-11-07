14c84b7918a7cc8d3650b2dbb2a5ceb89e0f1f02
David Goldberg <drgold1988@gmail.com>
2015-11-06 23:24:22 -0800

    [Feature] creates ProductSearch component

---
+++ client/src/components/ProductSearch.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render: function() {
+    return (
+      <div className="productSearch">
+        This is the ProductSearch component
+      </div>
+    );
+  }
+});
