6cfd77c84071ab35f010bb2e4aefc9614685ced8
David Goldberg <drgold1988@gmail.com>
2015-11-07 10:50:36 -0800

    [Feature] uses Redux connect to create a pure and connected verison of each component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 import App from './components/App';
-import Invoice from './components/Invoice';
-import ProductSearch from './components/ProductSearch';
+import {InvoiceContainer} from './components/Invoice';
+import {ProductSearchContainer} from './components/ProductSearch';
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
-  <Route path="/products" component={ProductSearch} />
-  <Route path="/" component={Invoice} />
+  <Route path="/products" component={ProductSearchContainer} />
+  <Route path="/" component={InvoiceContainer} />
 </Route>;
 
 ReactDOM.render(
-  <Router>{routes}</Router>,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
