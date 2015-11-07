799920b2c9d129c701e8059d319454649d105520
David Goldberg <drgold1988@gmail.com>
2015-11-06 23:24:41 -0800

    [Test] moves component test files to components foler

---
+++ client/test/components/ProductSearch_spec.jsx
@@ -0,0 +1,5 @@
+import React from 'react/addons';
+import {List} from 'immutable';
+import {expect} from 'chai';
+
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
