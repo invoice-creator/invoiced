446442713f5134322cba633553c032203cfd8590
David Goldberg <drgold1988@gmail.com>
2015-11-07 10:49:40 -0800

    [Feature] makes use of connected container version of Invoice component

--- client/src/components/Invoice.jsx
+++ client/src/components/Invoice.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
+export const Invoice = React.createClass({
   render: function() {
     return (
       <div className="invoice">
-        This is the Invoice component
+        {This is the Invoice component}
       </div>
     );
   }
 });
+
+function matStateToProps(state) {
+  return {
+    products: state.getIn(['products'])
+  }
+}
+
+export const InvoiceContainer = connect(
+  matStateToProps,
+  actionCreators
+)(Invoice);
