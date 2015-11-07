c69d849eeae0274417214ced608afb9c4a2277d2
David Goldberg <drgold1988@gmail.com>
2015-11-06 23:23:05 -0800

    [Feature] adds ProductSearch component to routing at /products

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
 import App from './components/App';
 import Invoice from './components/Invoice';
+import ProductSearch from './components/ProductSearch';
 
 const routes = <Route component={App}>
+  <Route path="/products" component={ProductSearch} />
   <Route path="/" component={Invoice} />
 </Route>;
 
