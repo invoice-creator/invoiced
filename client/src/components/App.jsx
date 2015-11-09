05e747b5f4abb6e25de406f1fcd0e3ebf6d0d6d6
David Goldberg <drgold1988@gmail.com>
2015-11-08 18:20:36 -0800

    [Feature] gets rid of mockData as the props are coming from Redux server store

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {List, Map} from 'immutable';
 
-const products = List.of('Body Wash', 'Hand Soap');
-
 export default React.createClass({
   render: function() {
     return this.props.children;
