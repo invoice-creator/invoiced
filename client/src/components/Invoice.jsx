c14de6836bf07c68b8facc712a3a99823897fbb6
David Goldberg <drgold1988@gmail.com>
2015-11-08 18:21:16 -0800

    [Feature] updates render method for Invoice.jsx

--- client/src/components/Invoice.jsx
+++ client/src/components/Invoice.jsx
@@ -7,19 +7,33 @@ export const Invoice = React.createClass({
   render: function() {
     return (
       <div className="invoice">
-        {This is the Invoice component}
+        <form>
+          <div className="form-group">
+            <label htmlFor="customerName">Customer Name</label>
+            <input type="text" className="form-control" id="customerNameInput" placeholder="Customer Name" onChange={this.handleCustomerNameChange} />
+          </div>
+          <div>
+            <label htmlFor="date">Date</label>
+            <input type="date" className="form-control" id="dateInput" placeholder="Date" onChange={this.handleDateChange} />
+          </div>
+          <div>
+            <label htmlFor="invoiceNumber">Invoice Number</label>
+            <input type="number" className="form-control" id="invoiceNumberInput" placeholder="Invoice Number" onChange={this.handleInvoiceNumberChange} />
+          </div>
+          <button type="submit" className="btn btn-default" onSubmit={this.handleSubmitInvoice}>Create Invoice</button>
+        </form>
       </div>
     );
   }
 });
 
-function matStateToProps(state) {
+function mapStateToProps(state) {
   return {
     products: state.getIn(['products'])
   }
 }
 
 export const InvoiceContainer = connect(
-  matStateToProps,
+  mapStateToProps,
   actionCreators
 )(Invoice);
