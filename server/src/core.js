a513ac568ed17c5d1c90a4035f150e75215d7e15
David Goldberg <drgold1988@gmail.com>
2015-11-09 19:12:25 -0800

    [Feature] updates core js to update of products state list sent to client

--- server/src/core.js
+++ server/src/core.js
@@ -3,5 +3,5 @@ import {List, Map} from 'immutable';
 export const INITIAL_STATE = Map();
 
 export function setProducts(state, products) {
-  return state.set('products', List(products));
+  return state.set('products', products.products);
 }
