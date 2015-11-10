34a7fb0adaa1f442ddb073b5a05e22073a576ccf
David Goldberg <drgold1988@gmail.com>
2015-11-09 22:10:34 -0800

    [Test] updates unit tests for core on server side

--- server/test/core_spec.js
+++ server/test/core_spec.js
@@ -9,10 +9,10 @@ describe('application logic', () => {
 
     it('adds the entries to the state', () => {
       const state = Map();
-      const products = List.of('Body Wash', 'Hand Soap');
+      const products = ['Body Wash', 'Hand Soap'];
       const nextState = setProducts(state, products);
       expect(nextState).to.equal(Map({
-        products: List.of('Body Wash', 'Hand Soap')
+        products: ['Body Wash', 'Hand Soap']
       }));
     });
 
@@ -25,23 +25,6 @@ describe('application logic', () => {
       }));
     });
 
-  });
-
-  describe('next', () => {
-
-    it('takes the selected products and moves them under invoiced', () => {
-      const state = Map({
-        products: List.of('Body Wash', 'Hand Soap', 'Shampoo')
-      });
-      const nextState = next(state);
-      expect(nextState).to.equal(Map({
-        invoiced: Map({
-          invoiceProducts: List.of('Body Wash', 'Hand Soap');
-        }),
-        products: List.of('Shampoo')
-      }));
-    });
-    
   });
 
 });
