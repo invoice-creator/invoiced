aac30cd5f6d0b76554834d4dccacdf855facf232
David Goldberg <drgold1988@gmail.com>
2015-11-09 22:13:14 -0800

    [Test] updates testing for client side

--- client/test/components/Invoice_spec.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react/addons';
-import {List} from 'immutable';
-import {Invoice} from '../../src/components/Invoice';
-import {expect} from 'chai';
-
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
--- client/test/components/ProductSearch_spec.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react/addons';
-import {List} from 'immutable';
-import {ProductSearch} from '../../src/components/ProductSearch';
-import {expect} from 'chai';
-
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
